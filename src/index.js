bf4516ecfd27b8f9cc589a67038b95be6a384369
il2407 <idolev53@gmail.com>
2023-02-16 11:57:46 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
